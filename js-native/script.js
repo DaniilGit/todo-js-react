1543e5bf36873cedff066a7b9d438c149113d00d
DaniilGit <daniil.work2019@gmail.com>
2021-06-14 10:48:38 +0700

    Check completed tasks, delete task, clear completed task

--- js-native/script.js
+++ js-native/script.js
@@ -1,44 +1,186 @@
 class Todo {
   constructor() {
     this.todoList = [];
+    this.counterActiveTasks = 0;
+    this.tasks = document.getElementById("tasks");
     this.todoForm = document.getElementById("todo-form");
     this.todoInput = document.getElementById("todo-input");
-    this.todoBtnCheckAll = document.getElementById("todo-btn-check");
+    this.btnCompleteAllTasks = document.getElementById("todo-btn-check");
+    this.btnClearCompletedTask = document.getElementById("clear-btn");
 
+    this.subsribeEvents();
+  }
+
+  subsribeEvents() {
     this.todoForm.onsubmit = () => {
       this.addNewTask();
+      this.checkNumberTasks();
       return false;
     };
+
+    this.tasks.onchange = () => {
+      this.checkCompletedTasks();
+    };
+
+    this.tasks.onclick = () => {
+      this.clearDeletedTasks();
+      this.checkNumberTasks();
+    };
+
+    this.btnCompleteAllTasks.onclick = () => {
+      this.checkedAllTasks();
+      this.checkCompletedTasks();
+    };
+
+    this.btnClearCompletedTask.onclick = () => {
+      this.clearCompletedTask();
+      this.checkNumberTasks();
+    }
   }
 
   addNewTask() {
     let textTask = this.todoInput.value;
     const task = new TodoTask(textTask);
-    this.todoList.push(task)
-    this.todoForm.append(task.task);
 
-    console.log(this.todoList);
+    this.counterActiveTasks++;
+    this.todoList.push(task);
+    this.todoInput.value = "";
+
+    this.renderTasks();
   }
 
-  clearCompletedTask() {}
+  renderTasks() {
+    this.todoList.forEach((task) => {
+      this.tasks.append(task.taskHtml)
+    })
+  }
+
+  checkNumberTasks() {
+    this.toggleVisibleButtonAndMenu();
+
+    if (this.todoList.length) {
+    } else {
+    }
+  }
+
+  checkedAllTasks() {
+    if (this.counterActiveTasks == 0) this.completeOrUncompleteAllTasks(false);
+    else this.completeOrUncompleteAllTasks(true);
+  }
+
+  completeOrUncompleteAllTasks(flagCompletedTask) {
+    this.todoList.forEach((task) => {
+      let checkboxTask = task.taskHtml.children[0].children[0];
+      checkboxTask.checked = flagCompletedTask;
+      task.completeTask(checkboxTask);
+    });
+  }
+
+  checkCompletedTasks() {
+    this.counterActiveTasks = 0;
+    this.todoList.forEach((task) => {
+      let blockTaskText = task.taskHtml.children[1];
+      if (task.isCompleted) blockTaskText.classList.add("completed");
+      else {
+        this.counterActiveTasks++;
+        blockTaskText.classList.remove("completed");
+      }
+    });
+  }
+
+  clearCompletedTask() {
+    let bufferList = [];
+    this.counterActiveTasks = 0;
+
+    this.todoList.forEach((task) => {
+      if (!task.isCompleted) {
+        bufferList.push(task)
+        this.counterActiveTasks++;
+      }
+    });
+
+    this.todoList = bufferList;
+    this.tasks.innerHTML = ''
+    this.renderTasks();
+  }
+
+  clearDeletedTasks() {
+    let bufferList = [];
+
+    this.todoList.forEach((task) => {
+      if (!task.isDeleted) {
+        bufferList.push(task)
+        if (!task.isCompleted) this.counterActiveTasks--;
+      }
+    });
+
+    this.todoList = bufferList;
+    this.tasks.innerHTML = ''
+    this.renderTasks();
+  }
+
+  toggleVisibleButtonAndMenu() {
+    const [todoBtnCheck, footerMenu] = [
+      document.getElementById("todo-btn-check"),
+      document.getElementById("footer-menu"),
+    ];
+
+    if (this.todoList.length) {
+      todoBtnCheck.classList.remove("hide");
+      footerMenu.classList.remove("hide");
+    } else {
+      todoBtnCheck.classList.add("hide");
+      footerMenu.classList.add("hide");
+    }
+  }
 }
 
 class TodoTask {
   constructor(text) {
     this.taskText = text;
     this.isCompleted = false;
-    this.task = document.createElement("div");
+    this.isDeleted = false;
 
-    this.task.classList.add("todo-task");
-    this.task.innerHTML = `
-      <input class="task-check" type="checkbox"></input>
-      <div class="task-text">${this.taskText}</div>
-      <button class="task-btn-delete">X</button>
-    `;
+    this.taskHtml = document.createElement("div");
+    this.taskHtml.classList.add("todo-task");
+    this.taskHtml.innerHTML = this.setHtmlPattern();
+
+    this.subsribeEvents();
   }
 
-  completeTask() {}
-  deleteTask() {}
+  subsribeEvents() {
+    this.taskHtml.addEventListener("click", (event) => {
+      let targetTask = event.target;
+
+      if (targetTask.classList.contains("task-checkbox-hide")) {
+        this.completeTask(targetTask);
+      }
+      if (targetTask.classList.contains("task-btn-delete")) {
+        this.deleteTask();
+      }
+    });
+  }
+
+  completeTask(targetTask) {
+    if (targetTask.checked) this.isCompleted = true;
+    else this.isCompleted = false;
+  }
+
+  deleteTask() {
+    this.isDeleted = true
+  }
+
+  setHtmlPattern() {
+    let id = this.taskText + Math.random();
+    return `
+      <div>
+        <input id="${id}" class="task-checkbox-hide" type="checkbox"></input>
+        <label class="task-checkbox" for="${id}"></label>
+      </div>
+      <div class="task-text">${this.taskText}</div>
+      <button class="task-btn-delete">&#10006;</button>
+    `;
+  }
 }
 
 const todo = new Todo();
